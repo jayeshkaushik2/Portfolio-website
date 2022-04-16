f1fcf75538bd660bc15d9569472b7eccbd9fb1c9
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-04-16 17:00:21 +0530

    fixes template location

--- jayesh_kaushik/settings.py
+++ jayesh_kaushik/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'front_end/build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
